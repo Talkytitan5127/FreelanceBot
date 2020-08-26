47ac5f54b4f971857fb46cc4d5f87649e4c46175
Gorbunov Pavel <p.gorbunov@corp.mail.ru>
2020-08-27 01:43:55 +0300

    add bot functions and done get_last_10_tasks function

---
+++ Bot/bot.py
@@ -0,0 +1,49 @@
+import logging
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, Dispatcher
+from telegram.ext import MessageHandler, CommandHandler
+from telegram.ext import CallbackContext, Filters
+from typing import List
+
+from parser import TaskListPage, ListItem
+
+# Enable Logging
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+class FreelanceBot:
+    def __init__(self, token):
+        self.__token = token
+        self.cursor = TaskListPage()
+        self.updater = Updater(token, use_context=True)
+
+    def echo(self, update: Update, context: CallbackContext):
+        logger.info('echo method')
+        context.bot.send_message(update.effective_chat.id, update.message.text)
+
+    def get_last_10_tasks(self, update: Update, context: CallbackContext):
+        logger.info('get last 10 tasks method')
+        tasks = []
+        try:
+            tasks: List[ListItem] = self.cursor.get_last_10_tasks()
+        except Exception as e:
+            logger.info('catch exception: ' + str(e))
+            context.bot.send_message(update.effective_chat.id, text='smth goes wrong, sorry')
+        for task in tasks:
+            keyboard = [[InlineKeyboardButton("description", callback_data=task.get_description())]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            context.bot.send_message(update.effective_chat.id, text=str(task),
+                                     reply_markup=reply_markup)
+
+    def __add_handler(self, dispatcher: Dispatcher):
+        dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), self.echo))
+        dispatcher.add_handler(CommandHandler('last_10_tasks', self.get_last_10_tasks))
+
+    def run(self):
+        self.__add_handler(self.updater.dispatcher)
+        self.updater.start_polling()
+        self.updater.idle()
